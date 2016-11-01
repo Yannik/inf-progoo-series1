37342f3d161398ed5fce7e48e2ca1926c8c879af
Yannik Sembritzki <yannik@sembritzki.me>
2016-11-01 13:01:17 +0100

    Solve polygon problem. Fixed the issue that Math.cos requires radians instead of degrees.

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,22 +1,22 @@
-import acm.graphics.GLine;
+import acm.graphics.*;
 import acm.program.*;
 
 public class Polygon extends GraphicsProgram{
     public void run() {
-        //int sides = readInt("Kanten: ");
-        int sides = 4;
+        int sides = readInt("Kanten: ");
         println("Generiere Polygon mit " + sides + " Kanten.");
         double totalDegrees = 360;
         double degreesPerSide = totalDegrees / sides;
         for (int side = 0; side < sides; side++) {
             double startDegree = degreesPerSide * side;
             double endDegree = degreesPerSide * (side + 1);
-            int xOffset = 0;
-            int yOffset = 0;
-            double x0 = Math.cos(startDegree) * 100 + xOffset;
-            double y0 = Math.sin(startDegree) * 100 + yOffset;
-            double x1 = Math.cos(endDegree) * 100 + xOffset;
-            double y1 = Math.sin(endDegree) * 100 + yOffset;
+            int xOffset = 150;
+            int yOffset = 150;
+            // use "/180*Math.PI" because Math.cos/Math.sin requires "radians" instead of degrees
+            double x0 = Math.cos(startDegree/180*Math.PI) * 100 + xOffset;
+            double y0 = Math.sin(startDegree/180*Math.PI) * 100 + yOffset;
+            double x1 = Math.cos(endDegree/180*Math.PI) * 100 + xOffset;
+            double y1 = Math.sin(endDegree/180*Math.PI) * 100 + yOffset;
             add(new GLine(x0, y0, x1, y1));
             println("Degree: " + startDegree + " endDegree: " + endDegree + " x0: " + x0 + " y0: " + y0 + " x1: " + x1 + " y1: " + y1);
         }
