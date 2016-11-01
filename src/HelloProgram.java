38f2938ce810c04867689cf5202f00369d2b2cd9
Yannik Sembritzki <yannik@sembritzki.me>
2016-11-01 10:37:02 +0100

    Solve task

---
+++ src/HelloProgram.java
@@ -0,0 +1,8 @@
+import acm.graphics.*;
+import acm.program.*;
+
+public class HelloProgram extends GraphicsProgram {
+    public void run() {
+        add(new GLabel("I'll be back."), 100, 75);
+    }
+}
